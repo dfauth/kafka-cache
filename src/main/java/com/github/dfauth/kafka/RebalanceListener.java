787b83a1a4cc651e9fb3a5203a5e45a27febd4e2
damian <dfauth@gmail.com>
2021-09-20 19:23:53 +0800

    bug fix

--- src/main/java/com/github/dfauth/kafka/RebalanceListener.java
+++ src/main/java/com/github/dfauth/kafka/RebalanceListener.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.TopicPartition;
 
 import java.time.ZonedDateTime;
 import java.util.Collection;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,11 @@ public interface RebalanceListener<K,V> extends KafkaConsumerAware<Consumer<Coll
 
     static <K,V> RebalanceListener<K,V> seekToTimestamp(ZonedDateTime t) {
         return c -> tps ->
-            c.offsetsForTimes(tps.stream().collect(Collectors.toMap(identity(), ignored -> t.toInstant().toEpochMilli())));
+                c.offsetsForTimes(tps.stream().collect(Collectors.toMap(identity(), ignored -> t.toInstant().toEpochMilli()))).forEach((tp,o) -> c.seek(tp,o.offset()));
+    }
+
+    static <K,V> RebalanceListener<K,V> seekToOffsets(Map<TopicPartition, Long> offsets) {
+        return c -> tps -> offsets.entrySet().stream().filter(e -> tps.contains(e.getKey())).forEach(e -> c.seek(e.getKey(), e.getValue()));
     }
 
     static <K,V> RebalanceListener<K,V> topicPartitionListener(Consumer<Collection<TopicPartition>> consumer) {
