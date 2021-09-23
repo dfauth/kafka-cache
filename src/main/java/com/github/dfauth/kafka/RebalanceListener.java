5b3822cf2358a302b4b06ff9645e227a1f4ee967
damian <dfauth@gmail.com>
2021-09-23 08:07:00 +0800

    bug fix to handle null offset values

--- src/main/java/com/github/dfauth/kafka/RebalanceListener.java
+++ src/main/java/com/github/dfauth/kafka/RebalanceListener.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.TopicPartition;
 import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -24,7 +25,8 @@ public interface RebalanceListener<K,V> extends KafkaConsumerAware<Consumer<Coll
 
     static <K,V> RebalanceListener<K,V> seekToTimestamp(ZonedDateTime t) {
         return c -> tps ->
-                c.offsetsForTimes(tps.stream().collect(Collectors.toMap(identity(), ignored -> t.toInstant().toEpochMilli()))).forEach((tp,o) -> c.seek(tp,o.offset()));
+                c.offsetsForTimes(tps.stream().collect(Collectors.toMap(identity(), ignored -> t.toInstant().toEpochMilli())))
+                        .forEach((tp,o) -> Optional.ofNullable(o).map(_o -> o.offset()).ifPresent(_o -> c.seek(tp,_o)));
     }
 
     static <K,V> RebalanceListener<K,V> seekToOffsets(Map<TopicPartition, Long> offsets) {
